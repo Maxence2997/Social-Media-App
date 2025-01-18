b626db87784d014ca7abe552ca46c4742fa3f36a
Maxence Yang <jyunanyang@gmail.com>
2025-01-18 10:59:13 +0800

    created Log In screen

---
+++ androidApp/src/main/java/idv/maxence2997/socialapp/android/auth/login/LogInViewModel.kt
@@ -0,0 +1,24 @@
+package idv.maxence2997.socialapp.android.auth.login
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class LogInViewModel : ViewModel() {
+  var uiState by mutableStateOf(LogInState())
+    private set
+
+  fun updateEmail(email: String) {
+    uiState = uiState.copy(email = email)
+  }
+
+  fun updatePassword(password: String) {
+    uiState = uiState.copy(password = password)
+  }
+}
+
+data class LogInState(
+  val email: String = "",
+  val password: String = "",
+)
