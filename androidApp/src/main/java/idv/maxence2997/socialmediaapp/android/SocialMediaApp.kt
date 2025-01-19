07be47e5b06b4de0c6f41b785c18aa0fc6a9cbc2
Maxence Yang <jyunanyang@gmail.com>
2025-01-19 15:37:42 +0800

    create SocialMediaApp Navigator

---
+++ androidApp/src/main/java/idv/maxence2997/socialmediaapp/android/SocialMediaApp.kt
@@ -0,0 +1,16 @@
+package idv.maxence2997.socialmediaapp.android
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.generated.NavGraphs
+
+@Composable
+fun SocialMediaApp() {
+  val navHostController = rememberNavController()
+
+
+
+
+  DestinationsNavHost(navGraph = NavGraphs.root, navController = navHostController)
+}
